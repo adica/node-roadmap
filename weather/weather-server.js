e20f889c46ea945813e57de0ac22995feb25d9bf
carmeladi <carmeladi@gmail.com>
2015-12-08 21:59:40 +0200

    add settings

--- weather/weather-server.js
+++ weather/weather-server.js
@@ -1,9 +1,24 @@
+var settings = require("./settings");
 var http = require("http");
-var server = http.createServer(function (request, response) {
-      response.writeHead(200, {"Content-Type": "text/html"});
-      response.write("hello server");
-      response.end();
-});
+var fs = require('fs');
 
-server.listen(80);
+var server = http.createServer(function(request, response) {
+    response.end();
+}).listen(9615);
+
 console.log("Server is listening");
+
+http.get(settings.weatherAPIurl + settings.citiesFileName, function(res) {
+    var body = '';
+
+    res.on('data', function(chunk) {
+    	console.log('chunk: '+ chunk)
+        body += chunk;
+    });
+
+    res.on('end', function() {
+        console.log('end!!')
+    });
+}).on('error', function(e) {
+    console.log("Got an error: ", e);
+});
