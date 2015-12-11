009cd6a7577328434d336791add81e9efbf229d9
adica <adi.carmel@sarine.com>
2015-12-11 17:04:30 +0000

    cities works

--- weather/weather-server.js
+++ weather/weather-server.js
@@ -1,24 +1,40 @@
 var settings = require("./settings");
 var http = require("http");
 var fs = require('fs');
+var zlib = require('zlib');
+var request = require("request");
 
-var server = http.createServer(function(request, response) {
-    response.end();
+var server = http.createServer(function(req, resp) {
+    //response.end();
 }).listen(9615);
 
-console.log("Server is listening");
+ var citiesBuffer = [];
+var _url = settings.weatherAPIurl + settings.citiesFileName;
+function getCities(url, callback) {
 
-http.get(settings.weatherAPIurl + settings.citiesFileName, function(res) {
-    var body = '';
 
-    res.on('data', function(chunk) {
-    	console.log('chunk: '+ chunk)
-        body += chunk;
+    http.get(url, function(res) {
+        // pipe the response into the gunzip to decompress
+        var gunzip = zlib.createGunzip();            
+        res.pipe(gunzip);
+
+        gunzip.on('data', function(data) {
+            // decompression chunk ready, add it to the buffer
+            citiesBuffer.push(data.toString())
+
+        }).on("end", function() {
+            // response and decompression complete, join the buffer and return
+            callback(null, citiesBuffer.join("")); 
+
+        }).on("error", function(e) {
+            callback(e);
+        })
+    }).on('error', function(e) {
+        callback(e)
     });
+}
 
-    res.on('end', function() {
-        console.log('end!!')
-    });
-}).on('error', function(e) {
-    console.log("Got an error: ", e);
+getCities(_url, function(err, data) {
+      if(err) console.log('err : ',err);
+      console.log('all cities : ' + data.length);
 });
