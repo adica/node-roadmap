653df57ee12ba50ec3ddd43e2561b68a0bfd0100
adica <adi.carmel@sarine.com>
2015-12-11 17:06:40 +0000

    remove request module

--- weather/weather-server.js
+++ weather/weather-server.js
@@ -2,7 +2,7 @@ var settings = require("./settings");
 var http = require("http");
 var fs = require('fs');
 var zlib = require('zlib');
-var request = require("request");
+
 
 var server = http.createServer(function(req, resp) {
     //response.end();
