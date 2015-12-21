e67156341b6e41414bd746e284f48768f7cb28b0
adica <carmeladi@gmail.com>
2015-12-21 18:25:01 +0200

    add session4

---
+++ session-4/server.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const settings = require("./settings");
+const net = require('net');
+const cities = {};
+const coords = {};
+const file = fs.readFileSync(path.join(__dirname,'data/city.list.json'), 'utf8');
+
+const arr = file.split('\n');
+
+arr.forEach(function (line) {
+	line = line.trim().replace(/\n/g, '');    
+    var c = JSON.parse(line);
+    cities[c.name] = c;
+    coords[c.coord.lon.toString() + "_" + c.coord.lat.toString()] = c;
+});
+
+
+var server = net.createServer(function(socket) {
+  socket.on('data', (data) => {
+    const evt = JSON.parse(data.toString('utf8'))
+    switch (evt.action) {
+      case 'getForecast':
+        console.log('getting forecast for city ', evt.city);
+        if (evt.city) {
+          socket.write('City valid');
+        } else {
+          socket.end('City not provided');
+        }
+      }
+    })
+});
+
+// grab a random port.
+server.listen({
+  port: settings.PORT
+}, function() {
+  address = server.address();
+  console.log("opened server on %j", address);
+});
+
+
+
+
+
